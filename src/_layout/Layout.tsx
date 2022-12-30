a27a1a2b4e6463e76300dc51f645cb5a972fdacc
NandawteK <nandawtek@gmail.com>
2022-12-30 23:06:52 +0100

    add error boundary

--- src/_layout/Layout.tsx
+++ src/_layout/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 
+import { ErrorBoundary } from "./ErrorBoundary";
+
 export function Layout() {
 	return (
 		<>
 			<header>
 				<h1>Gestion de Torneos Billar</h1>
 			</header>
-			<Outlet />
+			<ErrorBoundary>
+				<Outlet />
+			</ErrorBoundary>
 		</>
 	);
 }
