c662516abebde5fdec8d8e255d32e64d7ebd3a78
NandawteK <nandawtek@gmail.com>
2023-02-04 17:50:25 +0100

    production fixes

--- src/infrastructure/services/apis/billar_api/LeagueService.ts
+++ src/infrastructure/services/apis/billar_api/LeagueService.ts
@@ -3,7 +3,7 @@ import {
   LeagueInterface,
   LeagueServiceInterface,
 } from '../../../../domain/interfaces/league';
-import { leagues } from './mocks';
+// import { leagues } from './mocks';
 
 export class LeagueService implements LeagueServiceInterface {
   constructor(private readonly billarApiUrl: string) {}
@@ -11,18 +11,18 @@ export class LeagueService implements LeagueServiceInterface {
   async findAll(): Promise<LeagueInterface[]> {
     const url = `${this.billarApiUrl}/v1/league`;
 
-    return await leagues; // await request.get(url);
+    return await request.get(url);
   }
 
   async findById(id: string): Promise<LeagueInterface> {
     const url = `${this.billarApiUrl}/v1/league/${id}`;
 
-    return await leagues[1]; // await request.get(url);
+    return await request.get(url);
   }
 
   async create(data: Partial<LeagueInterface>): Promise<LeagueInterface> {
     const url = `${this.billarApiUrl}/v1/league`;
 
-    return await await request.post(url, data);
+    return await request.post(url, data);
   }
 }
